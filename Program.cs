e638a688665c4d4550eda0a5ae464029d51908e1
Zahrandi Rusrizal <Zahrandi@student.telkomuniversity.ac.id>
2022-03-26 10:19:38 +0700

    Menambahkan  dua class SayaTubeVideo dan SayaTubeUser

--- Program.cs
+++ Program.cs
@@ -8,7 +8,22 @@ namespace modul5_1302204080
         {
             SayaTubeUser aku = new SayaTubeUser("Zahrandi Rusrizal");
             SayaTubeVideo filmA = new SayaTubeVideo("A");
+            SayaTubeVideo filmB = new SayaTubeVideo("B");
+            SayaTubeVideo filmC = new SayaTubeVideo("C");
+            SayaTubeVideo filmD = new SayaTubeVideo("D");
+            SayaTubeVideo filmE = new SayaTubeVideo("E");
+            SayaTubeVideo filmF = new SayaTubeVideo("F");
+            SayaTubeVideo filmG = new SayaTubeVideo("G");
+            SayaTubeVideo filmH = new SayaTubeVideo("H");
             aku.addVideo(filmA);
+            aku.addVideo(filmB);
+            aku.addVideo(filmC);
+            aku.addVideo(filmD);
+            aku.addVideo(filmE);
+            aku.addVideo(filmF);
+            aku.addVideo(filmG);
+            aku.addVideo(filmH);
+
             aku.PrintAllVideoPlayCount();
         }
     }
